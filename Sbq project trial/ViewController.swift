187b1c4256818ea19f6d390f763e1991bd7fe658
Mai Abd Elmonem <mai.abdelmonem@admins-MacBook-Pro-3.local>
2019-10-17 13:16:15 +0200

    first commit

---
+++ Sbq project trial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sbq project trial
+//
+//  Created by Mai Abd Elmonem on 10/17/19.
+//  Copyright © 2019 Mai Abd Elmonem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
